f6216faf9b877a014152a3c9905e2e6ea5b68168
maaverik <nithinjosetom@gmail.com>
2021-05-23 18:39:13 +0530

    Installed ORM

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  let user = req.query.name;
+  if (!user) {
+    user = "World";
+  }
+  res.send("Hello " + user);
 });
 
 app.listen(4445, () => {
