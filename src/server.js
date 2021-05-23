90db21cc16a79a82756311361bb8a676c5835b0d
maaverik <nithinjosetom@gmail.com>
2021-05-23 18:55:53 +0530

    Set up basic route

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const { db } = require("./models/db");
+const linksRoute = require('./routes/links')
 
 const app = express();
 
-app.get("/", (req, res) => {
-	let user = req.query.name;
-	if (!user) {
-		user = "World";
-	}
-	res.send("Hello " + user);
-});
+app.use('/api/links/', linksRoute)
 
 db.sync()
 	.then(() => console.log("DB works"))
